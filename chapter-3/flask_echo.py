7be735c066be49fdbb7a381d6a6a6abf13d99b4e
Terry Bates <terryjbates@gmail.com>
2012-10-27 00:41:09 -0400

    Initial revision of flask echo program to do 'hello world'.

---
+++ chapter-3/flask_echo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Flask:
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/<input>")
+def hello(input):
+    return input
+
+if __name__ == "__main__":
+    app.run(debug=True)
